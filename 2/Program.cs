8ab0347cd47aed6990f9efba4f83e7719ded9968
Ilya <ilya_rogachev_89@mail.ru>
2022-03-12 19:22:01 +0300

    Task_2 ready

---
+++ 2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine("Введите целое положительное число");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int [n];
+
+for(int i = 0; i < array.Length; i++)
+{
+array[i] = i+1;
+    if(array[i]%2 == 0)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
