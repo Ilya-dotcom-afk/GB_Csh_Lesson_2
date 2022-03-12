830049defb5059afc13918ae964c8c171587747e
Ilya <ilya_rogachev_89@mail.ru>
2022-03-12 18:28:45 +0300

    Task_1 ready

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,26 @@
 ﻿// Показать числа от -N до N
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 0)
+{
+PositivPrint(n);
+}
+else{
+    NegativPrint(n);
+}
+
+void PositivPrint(int a){
+for(int i = -a; i <= a; i++)
+  {
+      Console.Write(i + " ");
+  }
+}
+
+void NegativPrint(int a){
+for(int i = -a; i >= a; i--)
+  {
+      Console.Write(i + " ");
+  }
+}
+
